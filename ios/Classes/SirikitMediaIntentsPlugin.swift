49a75d20d24d2d639cbbb51a4ec4b3ee41b783a9
nickshoe <nicolo.scarpa@gmail.com>
2025-01-27 18:09:05 +0100

    ios empty method handler

--- ios/Classes/SirikitMediaIntentsPlugin.swift
+++ ios/Classes/SirikitMediaIntentsPlugin.swift
@@ -2,18 +2,26 @@ import Flutter
 import UIKit
 
 public class SirikitMediaIntentsPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "sirikit_media_intents", binaryMessenger: registrar.messenger())
-    let instance = SirikitMediaIntentsPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let messenger = registrar.messenger()
+        let channel = FlutterMethodChannel(
+            name: "sirikit_media_intents",
+            binaryMessenger: messenger)
+        
+        let instance = SirikitMediaIntentsPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+
+    public func handle(
+        _ call: FlutterMethodCall, result: @escaping FlutterResult
+    ) {
+        switch call.method {
+        case "getPlatformVersion":
+            result("iOS " + UIDevice.current.systemVersion)
+        case "initialize":
+            result(FlutterMethodNotImplemented)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
 }
