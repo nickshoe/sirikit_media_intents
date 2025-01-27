c6bfb083c5c924cbc36f48f1f925adf9b6a9a2f7
nickshoe <nicolo.scarpa@gmail.com>
2025-01-27 16:06:14 +0100

    tests

--- test/sirikit_media_intents_test.dart
+++ test/sirikit_media_intents_test.dart
@@ -3,17 +3,26 @@ import 'package:sirikit_media_intents/sirikit_media_intents.dart';
 import 'package:sirikit_media_intents/sirikit_media_intents_platform_interface.dart';
 import 'package:sirikit_media_intents/sirikit_media_intents_method_channel.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+import 'package:sirikit_media_intents/types/media_intents_handler.dart';
 
 class MockSirikitMediaIntentsPlatform
     with MockPlatformInterfaceMixin
     implements SirikitMediaIntentsPlatform {
-
   @override
   Future<String?> getPlatformVersion() => Future.value('42');
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
 }
 
 void main() {
-  final SirikitMediaIntentsPlatform initialPlatform = SirikitMediaIntentsPlatform.instance;
+  final SirikitMediaIntentsPlatform initialPlatform =
+      SirikitMediaIntentsPlatform.instance;
+
+  test('Can be extended', () {
+    SirikitMediaIntentsPlatform.instance =
+        ExtendedSirikitMediaIntentsPlatform();
+  });
 
   test('$MethodChannelSirikitMediaIntents is the default instance', () {
     expect(initialPlatform, isInstanceOf<MethodChannelSirikitMediaIntents>());
@@ -21,9 +30,40 @@ void main() {
 
   test('getPlatformVersion', () async {
     SirikitMediaIntents sirikitMediaIntentsPlugin = SirikitMediaIntents();
-    MockSirikitMediaIntentsPlatform fakePlatform = MockSirikitMediaIntentsPlatform();
+    MockSirikitMediaIntentsPlatform fakePlatform =
+        MockSirikitMediaIntentsPlatform();
     SirikitMediaIntentsPlatform.instance = fakePlatform;
 
     expect(await sirikitMediaIntentsPlugin.getPlatformVersion(), '42');
   });
+
+  test(
+      'Default implementation of initialize() should throw unimplemented error',
+      () {
+    // Arrange
+    final ExtendedSirikitMediaIntentsPlatform platform =
+        ExtendedSirikitMediaIntentsPlatform();
+
+    // Act & Assert
+    expect(
+      () => platform.initialize(ExtendedMediaIntentsHandler()),
+      throwsUnimplementedError,
+    );
+  });
+}
+
+class ExtendedSirikitMediaIntentsPlatform extends SirikitMediaIntentsPlatform {}
+
+class ExtendedMediaIntentsHandler implements MediaIntentsHandler {
+  @override
+  void playMediaItemById(String mediaItemId) {
+    // TODO: implement playMediaItemById
+    throw UnimplementedError();
+  }
+
+  @override
+  String resolveMediaItemsId(String query) {
+    // TODO: implement resolveMediaItemsId
+    throw UnimplementedError();
+  }
 }
