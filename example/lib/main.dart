c28858a8278bc6fbdf136787310d5c4125a58392
nickshoe <nicolo.scarpa@gmail.com>
2025-01-29 10:46:30 +0100

    example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
@@ -6,25 +7,21 @@ import 'package:sirikit_media_intents/sirikit_media_intents.dart';
 import 'package:sirikit_media_intents/types/media_intents_handler.dart';
 
 void main() {
-  runApp(const MyApp());
+  final sirikitMediaIntentsPlugin = SirikitMediaIntents();
+  final mediaIntentsHandler = ExampleMediaIntentsHandler();
+
+  WidgetsFlutterBinding.ensureInitialized();
+  sirikitMediaIntentsPlugin.initialize(mediaIntentsHandler);
+
+  runApp(MyApp(
+    mediaIntentsHandler: mediaIntentsHandler,
+  ));
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  final ExampleMediaIntentsHandler mediaIntentsHandler;
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final _sirikitMediaIntentsPlugin = SirikitMediaIntents();
-
-  @override
-  void initState() {
-    super.initState();
-
-    _sirikitMediaIntentsPlugin.initialize(ExampleMediaIntentsHandler());
-  }
+  const MyApp({super.key, required this.mediaIntentsHandler});
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +31,77 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text('Running\n'),
+          child: Column(
+            children: [
+              Divider(),
+              StreamBuilder(
+                stream: mediaIntentsHandler.mediaSearch$,
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Text(
+                      'Ask Siri "Play something"',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    );
+                  }
+
+                  final mediaSearch = snapshot.data!;
+                  return Text(
+                    'Search by media name "${mediaSearch.mediaName}"',
+                  );
+                },
+              ),
+              StreamBuilder(
+                stream: mediaIntentsHandler.resolveMediaItems$,
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Text('Waiting for items to be resolved');
+                  }
+
+                  var resolvedMediaItemsWidgets = snapshot.data!
+                      .map(
+                        (mediaItem) => Text(
+                          '${mediaItem.identifier} - ${mediaItem.title} by ${mediaItem.artist}',
+                        ),
+                      )
+                      .toList();
+
+                  return Column(
+                    children: [
+                      Text('Resolved media items'),
+                      ...resolvedMediaItemsWidgets
+                    ],
+                  );
+                },
+              ),
+              Divider(),
+              StreamBuilder(
+                stream: mediaIntentsHandler.queuedMediaItems$,
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Text('Waiting for items to be queued');
+                  }
+
+                  final mediaItems = snapshot.data!;
+                  return Text(
+                    'Queued ${mediaItems.length} items',
+                  );
+                },
+              ),
+              StreamBuilder(
+                stream: mediaIntentsHandler.currentlyPlayingMediaItem$,
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Text('Waiting for an item to be played');
+                  }
+
+                  final mediaItem = snapshot.data!;
+                  return Text(
+                    'Playing ${mediaItem.identifier} - ${mediaItem.title} by ${mediaItem.artist}',
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -42,11 +109,27 @@ class _MyAppState extends State<MyApp> {
 }
 
 class ExampleMediaIntentsHandler extends MediaIntentsHandler {
+  final _mediaSearchSubject = StreamController<MediaSearch>();
+  Stream<MediaSearch> get mediaSearch$ => _mediaSearchSubject.stream;
+
+  final _resolvedMediaItemsSubject = StreamController<List<MediaItem>>();
+  Stream<List<MediaItem>> get resolveMediaItems$ =>
+      _resolvedMediaItemsSubject.stream;
+
+  final _currentlyPlayingMediaItemSubject = StreamController<MediaItem>();
+  Stream<MediaItem> get currentlyPlayingMediaItem$ =>
+      _currentlyPlayingMediaItemSubject.stream;
+
+  final _queuedMediaItemsSubject = StreamController<List<MediaItem>>();
+  Stream<List<MediaItem>> get queuedMediaItems$ =>
+      _queuedMediaItemsSubject.stream;
+
   @override
   List<MediaItem> resolveMediaItems(MediaSearch mediaSearch) {
-    // TODO: call backend APIs
+    _mediaSearchSubject.add(mediaSearch);
 
-    return [
+    // TODO: call backend APIs to find media items matching the search criteria
+    var mediaItems = [
       MediaItem(
         identifier: '<song-1-id>',
         title: 'Cool song 1',
@@ -60,16 +143,19 @@ class ExampleMediaIntentsHandler extends MediaIntentsHandler {
         artist: 'Cool Artist 2',
       )
     ];
+    _resolvedMediaItemsSubject.add(mediaItems);
+
+    return mediaItems;
   }
 
   @override
   void playMediaItems(List<MediaItem> mediaItems) {
-    final mediaItemToPlay = mediaItems.first;
-    final numOfMediaItemsToQueue = mediaItems.length - 1;
+    log('Queuing ${mediaItems.length} songs');
+    // TODO: call App media services for queueing media items
+    _queuedMediaItemsSubject.add(mediaItems);
 
-    log('Playing ${mediaItemToPlay.title} by ${mediaItemToPlay.artist}');
-    log('Queuing other $numOfMediaItemsToQueue songs');
-
-    // TODO: call App media services
+    log('Playing ${mediaItems.first.title} by ${mediaItems.first.artist}');
+    // TODO: call App media services for playing a media item
+    _currentlyPlayingMediaItemSubject.add(mediaItems.first);
   }
 }
