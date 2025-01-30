54d4c9b209497aaa95ec9c0fb772565b42dd698f
nickshoe <nicolo.scarpa@gmail.com>
2025-01-30 12:40:56 +0100

    documentation

--- lib/types/media_intents_handler.dart
+++ lib/types/media_intents_handler.dart
@@ -1,6 +1,10 @@
 import 'package:sirikit_media_intents/messages.g.dart';
 
 abstract class MediaIntentsHandler {
+  /// This method closely matches the semantic of the
+  /// `resolveMediaItems(for intent:INPlayMediaIntent, with completion:)`
+  /// method of the `INPlayMediaIntentHandling` protocol.
+  ///
   /// > Implement this method to resolve the media items to play. If your app
   /// > isn’t completely sure of the match, return a few results with the best
   /// > match first. The system can start playing the first item and offer the
@@ -9,6 +13,16 @@ abstract class MediaIntentsHandler {
   /// > &mdash; Official Apple Documentation
   Future<List<MediaItem>> resolveMediaItems(MediaSearch mediaSearch);
 
-  // TODO: move the iOS side logic to application(_:handle:completionHandler:) on the UIApplicationDelegate object?
+  /// Since the plugin adopts the in-app intent handling strategy, there are no
+  /// app extensions involved and the intents are handled directly in the app.
+  ///
+  /// For this reason, it is safe to consider `playMediaItems` method equivalent
+  /// to `application(_:handlerFor:)` method on the `UIApplicationDelegate`
+  /// class of your app, even if it actually gets executed in the
+  /// `handle(intent:INPlayMediaIntent, completion:)` method of the plugin's
+  /// class implementing the `INPlayMediaIntentHandling` protocol.
+  ///
+  /// The app will be launched in background and `playMediaItems` should begin
+  /// the audio playback.
   Future<void> playMediaItems(List<MediaItem> mediaItems);
 }
